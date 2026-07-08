1ed0335dad2dd3e993dd111d5c8f676627611e08
smsadat1 <smsadat788@gmail.com>
2026-07-09 00:06:12 +0600

    feat: Implement dispatcher subsystem http server

--- dispatcher/endpoints.go
+++ dispatcher/endpoints.go
@@ -1,23 +1,30 @@
 package dispatcher
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"shared"
 )
 
-var (
-	ResponseRootHanlderFn = ResponseRootHanlder
-	SubmissionRecieverFn  = SubmissionReciever
-	ValidateSubmissionFn  = ValidateSubmission
-)
+// encapsulates shared long term dependencies for http server
+type ServerEnv struct {
+	ctx  *context.Context
+	s3m  *shared.S3Manager
+	rmqm *shared.RMQManager
+}
 
-func ResponseRootHanlder(w http.ResponseWriter, r *http.Request) {
+func (env *ServerEnv) ResponseRootHanlder(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "AlpineJudge active")
 }
 
-func SubmissionReciever(w http.ResponseWriter, r *http.Request) {
+func (env *ServerEnv) SubmissionReciever(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -33,10 +40,10 @@ func SubmissionReciever(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// wrong submission
-	// if err = ValidateSubmissionFn(submission); err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	return
-	// }
+	if err = ValidateSubmission(*env.ctx, *env.s3m, submission); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	// successful submission
 	w.Header().Set("Content-Type", "application/json")
@@ -44,16 +51,73 @@ func SubmissionReciever(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"status": "success"})
 }
 
-func InitHttpServer() {
+func (env *ServerEnv) SSEHandler(w http.ResponseWriter, r *http.Request) {
+
+	// important SSE headers
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*") // change in PROD
+
+	// flush write
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	execEventQueue := make(chan amqp.Delivery)
+	if err := env.rmqm.Subscribe(*env.ctx, execEventQueue); err != nil {
+		http.Error(w, "Execution event queue failed!", http.StatusInternalServerError)
+		return
+	}
+
+	for {
+		select {
+		case <-r.Context().Done():
+			fmt.Println("Client disconnected.")
+			return
+
+		case e := <-execEventQueue:
+			// Format required by SSE: "data: <message>\n\n"
+			// double newline indicating the end of an event block
+			fmt.Fprintf(w, "Event: %v\n\n", e)
+
+			// flush immediately
+			flusher.Flush()
+		}
+	}
+}
+
+func (env *ServerEnv) ResultReciever(w http.ResponseWriter, r *http.Request) {
+
+	bucket := "ajbucket" // replace with env derived
+	job_id := "example"  // replace with url derived
+	key := "/submission/" + job_id + "/result.json"
+	resultFile := "result.json"
+
+	if err := env.s3m.DownloadFileFromS3(*env.ctx, bucket, key, resultFile); err != nil {
+		http.Error(w, "Failed fetching result\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func InitHttpServer(ctx context.Context, s3m *shared.S3Manager, rmqm *shared.RMQManager) {
+
+	env := &ServerEnv{
+		ctx:  &ctx,
+		s3m:  s3m,
+		rmqm: rmqm,
+	}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", ResponseRootHanlderFn)
-	mux.HandleFunc("POST /submit", SubmissionRecieverFn)
-	// mux.HandleFunc("GET /job/{job_id}/events")
-	// mux.HandleFunc("GET /jobs/{job_id}/result")
+	mux.HandleFunc("GET /", env.ResponseRootHanlder)
+	mux.HandleFunc("POST /submit", env.SubmissionReciever)
+	mux.HandleFunc("GET /job/{job_id}/events", env.SSEHandler)
+	mux.HandleFunc("GET /jobs/{job_id}/result", env.ResultReciever)
 
-	serverPort := "8080"
+	serverPort := ":8080"
 	fmt.Printf("Starting server on http://localhost%s\n", serverPort)
 
 	if err := http.ListenAndServe(serverPort, mux); err != nil {
---
+++ dispatcher/main.go
@@ -0,0 +1,51 @@
+package dispatcher
+
+import (
+	"context"
+	"log"
+	"os"
+	"shared"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := godotenv.Load("/etc/alpinejudge/dispatcher/dispatcher.env"); err != nil {
+		log.Fatal("Failed to load dispatcher env vars\n")
+	}
+
+	log.Println("Loading dispatcher configuration")
+	if err := LoadConfigs("/etc/alpinejudge/dispatcher/config.yaml"); err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// initiate s3
+	bucket := os.Getenv("MINIO_S3_BUCKET")
+	region := os.Getenv("S3_REGION_NAME")
+	accessKey := os.Getenv("S3_USERNAME_DEV")
+	secretKey := os.Getenv("S3_PASSWORD_DEV")
+	s3Endpoint := os.Getenv("S3_ENDPOINT_DEV")
+
+	s3Mgr, err := shared.InitS3Manager(ctx, bucket, region, accessKey, secretKey, s3Endpoint)
+	if err != nil {
+		log.Fatalf("Failed to spin up S3: %v", err)
+	}
+
+	// start rmq channel
+	amqpURL := os.Getenv("RABBITMQ_URL_DEV")
+	if amqpURL == "" {
+		log.Fatal("RMQ url not found in environment!\n")
+	}
+
+	rmqMgr, err := shared.NewRMQManager(ctx, amqpURL)
+	if err != nil {
+		log.Fatalf("Failed to spin up RabbitMQ: %v", err)
+	}
+	defer rmqMgr.Close()
+
+	// start http server
+	InitHttpServer(ctx, s3Mgr, rmqMgr)
+}
