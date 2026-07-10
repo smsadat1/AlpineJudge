23b803fcd7daa0b17d29e20d961446e86171adb0
smsadat1 <smsadat788@gmail.com>
2026-07-11 00:38:44 +0600

    feat: Implement in-container agent

---
+++ runner/executor/ajagent.go
@@ -0,0 +1,47 @@
+package executor
+
+import (
+	"encoding/json"
+	"fmt"
+	"local/runner/utils"
+	"log"
+	"os"
+	"os/exec"
+)
+
+// in-container agent to run tests
+
+func RunnerAgent() {
+
+	// find & load /workspace/execspec.json to spec
+	jsonData, err := os.ReadFile("/workspace/execspec.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var execSpec utils.AgentExecSpec
+
+	if err := json.Unmarshal(jsonData, &execSpec); err != nil {
+		log.Fatal(err)
+	}
+
+	// execute command & stream output to stdout
+	if execSpec.HasCompilePhase {
+		cmd := exec.Command("", execSpec.CompileArgs...)
+
+		// Capture stdout
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(string(output))
+	}
+
+	cmd := exec.Command("", execSpec.RunArgs...)
+	// Capture stdout
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(output))
+}
