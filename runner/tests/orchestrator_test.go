837a1db2b80f5fab266525fea9bc9e0a2f176043
smsadat1 <214529054+smsadat1@users.noreply.github.com>
2026-08-04 01:25:38 +0600

    test: Runner subsystem specific test

---
+++ runner/tests/orchestrator_test.go
@@ -0,0 +1,118 @@
+package tests
+
+import (
+	"assert"
+	"context"
+	"encoding/json"
+	"fmt"
+	"local/runner/pkg"
+	"log"
+	"os"
+	"testing"
+	"time"
+	"utils"
+
+	"github.com/containerd/containerd/v2/pkg/namespaces"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func Test_Orchestrator(t *testing.T) {
+
+	ctx, cancel := context.WithTimeout(t.Context(), 60*time.Second)
+	cCtx := namespaces.WithNamespace(ctx, "test-namespace")
+	defer cancel()
+
+	tr := pkg.NewRunnerTestRepository(t)
+	// tr.CreateTempLocations(t)
+	tf := pkg.NewRunnerTestFactory(t)
+	tf.StartTestMinioS3(t, ctx)
+	tf.StartTestRMQ(t, ctx)
+	tf.GetWarmContainer(t, cCtx)
+
+	// upload artifacts to S3 first ==============
+	srcFileData, err := os.Open("../examples/main.cpp")
+	if err != nil {
+		t.Fatalf("Failed to get source submission file: %v", err)
+	}
+	if err := tf.S3m.UploadFileToS3(ctx, tr.TestJobSpec.SrcCodeS3Key, srcFileData); err != nil {
+		t.Fatalf("Failed to upload source file: %v", err)
+	}
+	if err := tf.S3m.UploadDirToS3(ctx, tr.TestJobSpec.TestsetS3Key, "../examples/ts001"); err != nil {
+		t.Fatalf("Failed to upload testsests: %v", err)
+	}
+
+	// get events from RMQ
+	interceptorQueue := make(chan amqp.Delivery, 100)
+	if err := tf.Rmqm.Subscribe(ctx, interceptorQueue, tr.TestJobSpec.SSEQueue, "test-consoomer"); err != nil {
+		t.Fatalf("Failed to subscribe to queue: %v", err)
+	}
+
+	// channel to signal event end
+	eventsDone := make(chan struct{})
+
+	// Goroutine reading events cleanly with context cancellation
+	go func() {
+		defer close(eventsDone)
+		counter := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case delivery, ok := <-interceptorQueue:
+				if !ok {
+					return
+				}
+				_ = delivery.Ack(false)
+
+				var testEventStream utils.Event
+				json.Unmarshal(delivery.Body, &testEventStream)
+
+				log.Printf("Event type: %v | Status: %v | Details: %v | Stdout: %v | Stderr: %v",
+					testEventStream.Type, testEventStream.Status, testEventStream.Details, testEventStream.Stdout, testEventStream.Stderr)
+
+				counter++
+				assert.String(t, testEventStream.Status, fmt.Sprintf("Running test %v", counter))
+			}
+		}
+	}()
+
+	// channel to safely receive the return value of ExecSubm
+	type result struct {
+		info utils.ContainerInfo
+		err  error
+	}
+	execChan := make(chan result, 1)
+
+	go func() {
+		info, err := pkg.OrchestrateSubm(ctx, cCtx, tf.WarmContainer, *tf.S3m, tr.TestJobSpec, *tf.Rmqm)
+		execChan <- result{info: info, err: err}
+	}()
+
+	// wait for OrchestrateSubm() to complete
+	var contInfo utils.ContainerInfo
+	var orchError string
+	select {
+	case result := <-execChan:
+		contInfo = result.info
+		orchError = fmt.Sprintf("%v", result.err)
+	case <-ctx.Done():
+		t.Error("Timed out waiting for execution submission")
+	}
+
+	<-eventsDone
+
+	t.Logf(`Container lifecycle data
+			SubmissionID: %v
+			Language: %v
+			Version: %v
+			Elapsed time: %vms
+			Status: %v
+			StatusInfo: %v
+			Stderr: %v
+			Stdout: %v
+			Orhcestration error: %v`,
+		contInfo.SubmissionId, contInfo.Language, contInfo.Version,
+		contInfo.Interval, contInfo.Status, contInfo.StatusInfo, contInfo.ContainerStderr, contInfo.ContainerStdout,
+		orchError)
+
+}
---
+++ runner/tests/runner_integration_test.go
@@ -0,0 +1,127 @@
+package tests
+
+import (
+	"assert"
+	"context"
+	"encoding/json"
+	"fmt"
+	"local/runner/pkg"
+	"log"
+	"os"
+	"testing"
+	"time"
+	"utils"
+
+	"github.com/containerd/containerd"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func Test_InitRunner(t *testing.T) {
+	ctx, cancel := context.WithTimeout(t.Context(), 60*time.Second)
+	defer cancel()
+
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		t.Fatalf("Failed connecting containerd client: %v", err)
+	}
+
+	tr := pkg.NewRunnerTestRepository(t)
+	tf := pkg.NewRunnerTestFactory(t)
+	tf.StartTestMinioS3(t, ctx)
+	tf.StartTestRMQ(t, ctx)
+
+	// upload artifacts to S3 first ==============
+	srcFileData, err := os.Open("../examples/main.cpp")
+	if err != nil {
+		t.Fatalf("Failed to get source submission file: %v", err)
+	}
+	if err := tf.S3m.UploadFileToS3(ctx, tr.TestJobSpec.SrcCodeS3Key, srcFileData); err != nil {
+		t.Fatalf("Failed to upload source file: %v", err)
+	}
+	if err := tf.S3m.UploadDirToS3(ctx, tr.TestJobSpec.TestsetS3Key, "../examples/ts001"); err != nil {
+		t.Fatalf("Failed to upload testsests: %v", err)
+	}
+
+	// get events from RMQ
+	interceptorQueue := make(chan amqp.Delivery, 100)
+	if err := tf.Rmqm.Subscribe(ctx, interceptorQueue, tr.TestJobSpec.SSEQueue, "test-consoomer"); err != nil {
+		t.Fatalf("Failed to subscribe to queue: %v", err)
+	}
+
+	// channel to signal event end
+	eventsDone := make(chan struct{})
+
+	// Goroutine reading events cleanly with context cancellation
+	go func() {
+		defer close(eventsDone)
+		counter := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case delivery, ok := <-interceptorQueue:
+				if !ok {
+					return
+				}
+				_ = delivery.Ack(false)
+
+				var testEventStream utils.Event
+				json.Unmarshal(delivery.Body, &testEventStream)
+
+				log.Printf("Event type: %v | Status: %v | Details: %v | Stdout: %v | Stderr: %v",
+					testEventStream.Type, testEventStream.Status, testEventStream.Details, testEventStream.Stdout, testEventStream.Stderr)
+
+				counter++
+				assert.String(t, testEventStream.Status, fmt.Sprintf("Running test %v", counter))
+			}
+		}
+	}()
+
+	// channel to safely receive the return value of ExecSubm
+	type result struct {
+		info utils.ContainerInfo
+		err  error
+	}
+	execChan := make(chan error, 1)
+
+	// send jobspec payload over rmq
+	jobPayload, err := json.Marshal(tr.TestJobSpec)
+	if err != nil {
+		t.Fatalf("Failed marshaling jobspec payload: %v", err)
+	}
+	msg := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        jobPayload,
+	}
+	if err := tf.Rmqm.Publish(ctx, "later", msg); err != nil {
+		t.Fatalf("Failed sending jobspec payload over rmq: %v", err)
+	}
+
+	deps := utils.EngineDeps{
+		Client:    client,
+		Rmq:       tf.Rmqm,
+		S3:        tf.S3m,
+		JobQueue:  "later",
+		SSEQueue:  tr.TestJobSpec.SSEQueue,
+		Namespace: "test-runner",
+	}
+
+	go func() {
+		pkg.InitRunner(ctx, deps)
+		execChan <- err
+	}()
+
+	// wait for OrchestrateSubm() to complete
+	// var orchError string
+	// select {
+	// case result := <-execChan:
+	// 	orchError = fmt.Sprintf("%v", result)
+	// case <-ctx.Done():
+	// 	t.Error("Timed out waiting for execution submission")
+	// }
+
+	<-eventsDone
+
+	// t.Logf(`Orhcestration error: %v`, orchError)
+
+}
