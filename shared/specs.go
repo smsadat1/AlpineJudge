c7e6c84aae71226933d1e396f93a6375532242f9
smsadat1 <214529054+smsadat1@users.noreply.github.com>
2026-08-18 21:25:38 +0600

    refact: Update JobSpec to support per test resource limitation

--- shared/specs.go
+++ shared/specs.go
@@ -2,17 +2,14 @@ package shared
 
 type JobSpec struct {
 	Language             string `json:"language"`
-	Version              string `json:"version"`
-	Image                string `json:"image"`
 	Source               string `json:"source"`
 	SubmissionID         string `json:"submission_id"`
 	Bucket               string `json:"s3_bucket"`
 	SrcCodeS3Key         string `json:"src_code_s3key"`
 	TestsetS3Key         string `json:"testset_s3Key"`
 	Testset              string `json:"testset_id"`
-	TestsetVersion       string `json:"testset_version"`
 	SSEQueue             string `json:"sse_queue"`
-	PerTestMemoryLimitMB uint64 `json:"per_test_memory_limit_mb"`
-	PerTestTimeoutsec    uint32 `json:"per_test_timeout_sec"`
-	PerTestLogLimitKB    uint32 `json:"per_test_log_limit_kb"`
+	PerTestMemoryLimitMB uint64 `json:"memory_limit_mb"`
+	PerTestTimeoutsec    uint32 `json:"timeout_sec"`
+	PerTestLogLimitKB    uint32 `json:"log_limit_kb"`
 }
